229479ade4f550f8ced367d4f6f30280f05d1b96
MilanRadeta <milan.radeta.peca@gmail.com>
2022-02-06 17:10:10 +0100

    Use position as public prop

--- Assets/Scripts/Cards/CardDescription.cs
+++ Assets/Scripts/Cards/CardDescription.cs
@@ -7,6 +7,7 @@ public class CardDescription : MonoBehaviour
     public static CardDescription instance;
     public Transform cardSlot;
     public TMPro.TextMeshPro text;
+    public Vector3 position = new Vector3(0, 0.75f, 2.5f);
 
     public CardDescription()
     {
@@ -19,7 +20,7 @@ public class CardDescription : MonoBehaviour
 
     public void Show(Card card)
     {
-        var position = new Vector3(0, 0.75f, 2.5f);
+        var position = new Vector3(this.position.x, this.position.y, this.position.z);
         if (card.transform.position.z > 0)
         {
             position.y = -position.y;
@@ -36,7 +37,6 @@ public class CardDescription : MonoBehaviour
         newCard.transform.localRotation = Quaternion.Euler(Vector3.zero);
         newCard.transform.localScale = Vector3.one;
         
-        Debug.Log(card.description);
         text.text = card.description;
 
 
