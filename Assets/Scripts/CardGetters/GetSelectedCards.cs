30e46646ac999ec5ec15d58f4bafa915928d3ef2
MilanRadeta <milan.radeta.peca@gmail.com>
2022-02-25 13:14:50 +0100

    Use select cards in getselectedcards

--- Assets/Scripts/CardGetters/GetSelectedCards.cs
+++ Assets/Scripts/CardGetters/GetSelectedCards.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class SelectCards : FilterCards
+public class GetSelectedCards : GetCards
 {
-    public CardCondition condition;
-    public int count = 1;
-    protected List<Card> selected;
-    private bool clicked;
+    public SelectCards SelectCardsAction;
 
     void OnValidate()
     {
-        if (CardsGetter == null)
+        if (SelectCardsAction == null)
         {
-            throw new MissingReferenceException("SelectCards.CardsGetter not assigned on " + gameObject.name);
+            throw new MissingReferenceException("GetSelectedCards.SelectCardsAction not assigned on " + gameObject.name);
         }
     }
 
@@ -22,50 +19,7 @@ public class SelectCards : FilterCards
     {
         get
         {
-            selected = new List<Card>();
-            var confirmed = false;
-            while (!confirmed)
-            {
-                clicked = false;
-                var cards = Cards.Where(c => CanSelect(c));
-                cards.ToList().ForEach(c => c.OnClicked += Select);
-                selected.ForEach(c => c.OnClicked += Deselect);
-                // yield return new WaitUntil(() => clicked);
-                // TODO show choose dialog
-
-                cards.ToList().ForEach(c => c.OnClicked -= Select);
-                selected.ForEach(c => c.OnClicked -= Deselect);
-            }
-            return selected;
+            return SelectCardsAction.Selected;
         }
-    }
-
-    private void Select(Card c)
-    {
-        selected.Add(c);
-        clicked = true;
-    }
-
-    private void Deselect(Card c)
-    {
-        selected.Remove(c);
-        clicked = true;
-    }
-
-    private bool CanSelect(Card c)
-    {
-        return !selected.Contains(c)
-            && selected.Count < count
-            && condition.SatisfiesRequirements(selected.Concat(new List<Card>() { c })); ;
-    }
-
-    private bool CanDeselect(Card card)
-    {
-        return selected.Contains(card);
-    }
-
-    private bool CanConfirm()
-    {
-        return condition.SatisfiesRequirements(selected);
     }
 }
